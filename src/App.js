b7b2dbcf413126e3e23d7d4751b323eca056d3e2
jas8157 <jas8157@naver.com>
2023-10-20 17:11:31 +0900

    api를 이용한 현재위치 날씨정보 가져오기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const lat = position.coords.latitude
+      const lon = position.coords.longitude
+      getWeatherByCurrentLocation(lat, lon);
+    });
+  };
+
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=f8c24d1da725479cbb3dfdc91abfb54a`
+    const response = await fetch(url)
+    const data = await response.json();
+    console.log("data", data)
+  };
+
+  useEffect(() => {
+    getCurrentLocation()
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
     </div>
   );
 }
